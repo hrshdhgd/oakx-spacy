91cf248ba9ce1eadb247dfdd704d29d3dfa1d061
Harshad Hegde <hegdehb@gmail.com>
2022-12-30 14:50:35 -0600

    fixed if condition for unittest

--- tests/test_spacy_implementation.py
+++ tests/test_spacy_implementation.py
@@ -20,7 +20,7 @@ class TestSpacyImplementation(unittest.TestCase):
         self.config = TextAnnotationConfiguration()
 
     @unittest.skipIf(
-        os.getenv("GITHUB_ACTIONS") == True,
+        os.getenv("GITHUB_ACTIONS"),
         "Avoid: Got SIGTERM, handling it as a KeyboardInterrupt",
     )
     def test_annotate_file(self):
