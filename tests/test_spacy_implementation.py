8fb80bced9f08a1c93155ab8ba198189a6581010
Harshad Hegde <hegdehb@gmail.com>
2022-12-30 15:50:08 -0600

    switching model = changed ner count

--- tests/test_spacy_implementation.py
+++ tests/test_spacy_implementation.py
@@ -26,11 +26,11 @@ class TestSpacyImplementation(unittest.TestCase):
     def test_annotate_file(self):
         """Test annotation of a file."""
         results = list(self.impl.annotate_file(self.input_file, self.config))
-        self.assertEqual(len(results), 20)
+        self.assertEqual(len(results), 15)
         self.assertTrue("C1551088" in [x.subject_text_id for x in results])
 
     def test_annotate_text(self):
         """Test annotation of text."""
         results = list(self.impl.annotate_text(self.input_words, self.config))
-        self.assertEqual(len(results), 13)
+        self.assertEqual(len(results), 9)
         self.assertTrue("C0054952" in [x.subject_text_id for x in results])
